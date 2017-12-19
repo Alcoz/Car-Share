58c86998dc915e7910b72f590dc64f61497f7b20
Moonstaarz <eliott.duverger@hotmail.fr>
2017-12-19 22:06:26 +0100

    Proposer_traj

---
+++ projetCovoiturage/proposertrajat.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+ ?>
+<!Doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styles/connexion.css" />
+    <title>Car-Share - Accueil</title>
+  </head>
+  <body>
+      <nav>
+        <ul id="menu">
+          <a id="acc" href="accueil.php">  <li> Car-Share </li> </a>
+          <?php
+          if (isset($_SESSION['id'])) {
+            echo "<a href=\"connexion.php\"> <li> Deconnexion</li> </a>";
+            echo "<a href=\"proposertrajat.php\">  <li>Proposer un trajet  </li> </a>";
+          }
+          else {
+            echo "<a href=\"inscription.php\">  <li> Inscription  </li> </a>";
+
+            echo "<a href=\"connexion.php\">  <li> Connexion </li> </a>";
+          }
+           ?>
+        </ul>
+      </nav>
+      <div id="corps_menu">
+      </div>
+      <?php
+      if (isset($_SESSION['id'])) {
+        ?>
+        <div id="formul">
+          <form method="post" action = "post_propose.php">
+            <label>  Départ :</label> <input id="home" type="text" name = "depart"/>
+            <label> Destination :</label> <input  id="curs" type="text" name = "destination"/>
+            <label> Date : </label> <input  id="calen" type="date" name = "date"/>
+            <input id="valider" type="submit" name = "recherche"/>
+          </form>
+        </div>
+
+        <?php
+        $pdo = new PDO('mysql:host=localhost;dbname=carshare;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $query = $pdo->query("SELECT *
+                              FROM TRAJET
+                              WHERE TYPE = 1
+                              ;"
+                            );
+        $tuples= $query->fetchAll(PDO::FETCH_OBJ);
+
+      }
+      else{
+        echo "<div> <p> Vous devez êtres connecté pour proposer un trajt </p>";
+      }
--- projetCovoiturage/proposition.php
+++ projetCovoiturage/proposition.php
@@ -5,10 +5,28 @@ session_start();
 <html>
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="stylesheet.css" />
-    <title>Page de proposition</title>
+    <link rel="stylesheet" href="styles/connexion.css" />
+    <title>Car-Share - Accueil</title>
   </head>
   <body>
+      <nav>
+        <ul id="menu">
+          <a id="acc" href="accueil.php">  <li> Car-Share </li> </a>
+          <?php
+          if (isset($_SESSION['id'])) {
+            echo "<a href=\"connexion.php\"> <li> Deconnexion</li> </a>";
+            echo "<a href=\"proposition.php\">  <li>Proposer un trajet  </li> </a>";
+            echo "<a href=\"profil.php\">  <li>Mon profil  </li> </a>";
+          }
+          else {
+            echo "<a href=\"inscription.php\">  <li> Inscription  </li> </a>";
+            echo "<a href=\"connexion.php\">  <li> Connexion </li> </a>";
+          }
+           ?>
+        </ul>
+      </nav>
+      <div id="corps_menu">
+      </div>
     <div class="proposition">
       <?php
         try{
@@ -26,22 +44,30 @@ session_start();
           $prix = $_POST['prix'];
           $date_dep = $_POST['date_dep'];
           $id_voiture = $_POST['id_voiture'];
+          $id_cond = $_SESSION['id'];
 
-          $pdo->exec("INSERT INTO TRAJET(TYPE, VILLE_DEP, ADRESSE_DEPART, VILLE_ARR, ADRESSE_ARR, DISTANCE, PRIX, DATE_DEP, DISPONIBLE, ID_VOITURE, ID_CONDUCTEUR) VALUES(TRUE,'".$ville_dep."','".$ad_dep."','".$ville_arr."','".$ad_arr."',".$distance.",".$prix.",".$date_dep.", 0,".$id_voiture.",".$_SESSION['id'].");");
+
+          $pdo = new PDO('mysql:host=localhost;dbname=carshare;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+
+
+          $query = $pdo->exec("INSERT INTO TRAJET (TYPE, VILLE_DEP, ADRESSE_DEPART, VILLE_ARR, ADRESSE_ARR, DISTANCE, PRIX, DATE_DEP, DISPONIBLE, ID_VOITURE, ID_CONDUCTEUR)
+                  VALUES ('1', '$ville_dep', '$ad_dep', '$ville_arr', '$ad_arr', '$distance', '$prix', '$date_dep', '1', '$id_voiture', '$id_cond');");
           header('Location: accueil.php');
         }
-
-        echo "<h2>Inscription<h2>";
-        echo "<form action=\"{$_SERVER['PHP_SELF']}\" method=\"post\">";
-        echo "Ville de depart<input type='text' name='ville_dep' \></br>";
-        echo "Adresse de depart<input type='text' name='ad_dep' \></br>";
-        echo "Ville d'arrivée<input type='text' name='ville_arr' \></br>";
-        echo "Adresse d'arrivée<input type='text' name='ad_arr' \></br>";
-        echo "Distance<input type='number' name='distance'/></br>";
-        echo "Prix<input type='number' name='prix'/></br>";
-        echo "Date de depart<input type='date' name='date_dep' \></br>";
-        echo "Voiture<input type='number' name='id_voiture' \></br>";
-        echo "<input type='submit' value='Propositon'/>";
+        else {
+          echo "<h2>Inscription<h2>";
+          echo "<form action=\"{$_SERVER['PHP_SELF']}\" method=\"post\">";
+          echo "Ville de depart<input type='text' name='ville_dep' \></br>";
+          echo "Adresse de depart<input type='text' name='ad_dep' \></br>";
+          echo "Ville d'arrivée<input type='text' name='ville_arr' \></br>";
+          echo "Adresse d'arrivée<input type='text' name='ad_arr' \></br>";
+          echo "Distance<input type='number' name='distance'/></br>";
+          echo "Prix<input type='number' name='prix'/></br>";
+          echo "Date de depart<input type='date' name='date_dep' \></br>";
+          echo "Voiture<input type='number' name='id_voiture' \></br>";
+          echo "<input type='submit' value='Propositon'/>";
+        }
       ?>
     </div>
   </body>
