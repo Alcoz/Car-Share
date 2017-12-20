09b149a60af45139406d28719386ba3e13f2bd92
Moonstaarz <eliott.duverger@hotmail.fr>
2017-12-20 03:53:07 +0100

    Un peu de tout (presque inscription trajet)

--- projetCovoiturage/trajet.php
+++ projetCovoiturage/trajet.php
@@ -28,7 +28,65 @@ session_start();
       <div id="corps_menu">
       </div>
       <?php
-      echo "Le trajet selectionné est ".$_POST['in_traj']; ?>
+      if (isset($_POST['imin'])) {
+        echo "LAAAAAAAAAAAAAAA";
+        $id_final_uti = $_SESSION['id'];
+        $id_final_traj = $_POST['trajprit'];
+        $query = $pdo->exec("INSERT INTO FAIT_TRAJET (ID_TRAJET, ID_PASSAGER)
+                VALUES ('$id_final_traj', '$id_final_uti');");
+        header('Location: accueil.php');
+      }
+
+
+      $id_traj = $_POST['id_trajet'];
+      $pdo = new PDO('mysql:host=localhost;dbname=carshare;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+      $query = $pdo->query("SELECT *
+                            FROM TRAJET
+                            WHERE ID_TRAJET = \"$id_traj\";");
+
+      $tuples= $query->fetchAll(PDO::FETCH_OBJ);
+
+
+
+      echo "<div id=\"desc\">";
+      foreach ($tuples as $tuple) {
+        $id_cond = $tuple->ID_CONDUCTEUR;
+
+        $query2 = $pdo->query("SELECT *
+                              FROM UTILISATEUR
+                              WHERE ID_UTILISATEUR = \"$id_cond\";");
+
+        $tuples2= $query2->fetchAll(PDO::FETCH_OBJ);
+        foreach ($tuples2 as $tuple2) {
+
+                  echo "<div>";
+                  echo "<p> Trajet : ".$tuple->VILLE_DEP."  ----->  ".$tuple->VILLE_ARR."</p>";
+                  echo "<p id=\"p2\"> Nombre de place restante : ";
+                  echo "</div>";
+
+                  echo "<div>";
+                  echo "<p> De : ".$tuple->ADRESSE_DEPART."</p>";
+                  echo "</div>";
+
+                  echo "<div>";
+                  echo "<p> A : ".$tuple->ADRESSE_ARR."</p>";
+                  echo "<p id=\"p2\"> le ".$tuple->DATE_DEP." </p>";
+                  echo "</div>";
+
+                  echo "<div>";
+                  echo "<p> Conducteur : ".$tuple2->PRENOM." ".$tuple2->NOM."</p>";
+                  echo "<p id=\"p2\">  Prix : ".$tuple->PRIX."€ </p>";
+                  echo "</div>";
+
+                  echo "<div id=\"inscription\">";
+                  echo "<from method=\"POST\" action=\"trajet.php\"> <input type=\"hidden\" value=\"".$id_traj."\" name=\"trajprit\"> <input type=\"submit\" value=\"M'inscrire\" name=\"imin\">";
+                  echo "</div>";
+
+        }
+
+
+      }?>
+    </div>
 
     </body>
     </html>
